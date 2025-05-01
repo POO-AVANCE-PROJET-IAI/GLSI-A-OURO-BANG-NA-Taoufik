efa73dfbf2a3a324edc9fd5ad3f82d6e20711539
taoufikOURO <ourotaoufik1@gmail.com>
2025-05-01 21:00:10 +0000

    CRUD des actes

--- OuroBangnaTaoufikAkhira/urls.py
+++ OuroBangnaTaoufikAkhira/urls.py
@@ -20,4 +20,12 @@ urlpatterns = [
     path("acttypes/<int:pk>/edit/", views.acttype_edit, name="acttypes.edit"),
     path("acttypes/<int:pk>/update/", views.acttype_update, name="acttypes.update"),
     path("acttypes/<int:pk>/destroy/", views.acttype_destroy, name="acttypes.destroy"),
+    # ========== GESTION DES ACTES ==========
+    path("acts/", views.act_index, name="acts.index"),
+    path("acts/<int:pk>/", views.act_show, name="acts.show"),
+    path("acts/create/", views.act_create, name="acts.create"),
+    path("acts/store/", views.act_store, name="acts.store"),
+    path("acts/<int:pk>/edit/", views.act_edit, name="acts.edit"),
+    path("acts/<int:pk>/update/", views.act_update, name="acts.update"),
+    path("acts/<int:pk>/destroy/", views.act_destroy, name="acts.destroy"),
 ]
